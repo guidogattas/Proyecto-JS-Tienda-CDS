af306382566f8e212cc9ba7031aa55a913b77b48
guidogattas <112128131+guidogattas@users.noreply.github.com>
2023-03-09 18:39:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 async function getProductos() { 
   
-  const response = await fetch("productos.js");
+  const response = await fetch("productos.json");
   const data = await response.json();
   return data;
 
